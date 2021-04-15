f9025b73e085f504f0e4d167138cb2397a407506
Tim Mortimer <tim@kiteframe.co.uk>
2021-04-15 17:36:21 +0100

    Return "application/json" Content-Type from CreateTodo

--- HelloWorldFunction/src/main/java/todos/CreateTodo.java
+++ HelloWorldFunction/src/main/java/todos/CreateTodo.java
@@ -3,11 +3,14 @@ package todos;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import java.util.Map;
 
 public class CreateTodo {
 
   public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final
       Context context) {
-    return new APIGatewayProxyResponseEvent().withStatusCode(201);
+    return new APIGatewayProxyResponseEvent()
+        .withHeaders(Map.of("Content-Type", "application/json"))
+        .withStatusCode(201);
   }
 }
